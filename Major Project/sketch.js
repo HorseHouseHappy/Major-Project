f4c3b4a609cdc335df0f92d6bcced82fe89efa46
HorseHouseHappy <BacktoHongKongLongSchlong@gmail.com>
2021-02-14 19:29:21 -0600

    began adding more sprites, movement, and framework to display sprites

--- Major Project/sketch.js
+++ Major Project/sketch.js
@@ -5,28 +5,115 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let wall, stoneWall, skeleton;
+let floor, wall, stoneWall, skeleton, player, coins;
 let levelBackground;
 let tiles;
 let tilesHigh, tilesWide;
 let tileWidth, tileHeight;
 let levelToLoad;
 let lines;
+let playerX = 0;
+let playerY = 0;
 
 function preload() {
   // loading level from txt file 
   levelToLoad = "level/e1m1.txt"
   lines = loadstrings(levelToLoad);
 
-  levelBackground = loadImage("assets/sprites/e1m1_floor.png");
-
+  floor = loadImage("assets/sprites/e1m1_floor.png");
+  player = loadImage("assets/sprites/cadence.gif");
+  // load other sprites as needed
   
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(1000, 750);
+  tilesHigh = lines.length;
+  tilesWide = lines[0].length;
+
+  tileWidth = width / tilesWide;
+  tileHeight = height / tilesHigh;
+
+  tiles = createEmpty2dArray(tilesWide, tilesHigh);
+
+  for (let y = 0; y < tilesHigh; y++) {
+    for (let x = 0; x < tilesWide; x++) {
+      let tileType = lines[y][x];
+      tiles[x][y] = tileType;
+    }
+  }
 }
 
 function draw() {
   background(255);
+}
+
+function createEmpty2dArray(cols, rows) {
+  let randomGrid = [];
+  for (let x = 0; x < cols; x++) {
+    randomGrid.push([]);
+    for (let y = 0; y < rows; y++) {
+      randomGrid[x].push(0);
+    }
+  }
+  return randomGrid;
+}
+
+function showTile(location, x, y) {
+  if (location === "#") {
+    image(wall, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+  }
+  else if (location === "C") {
+    image(coin, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+  }
+  else if (location === "B") {
+    image(skeleton, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+  }
+  else if (location === "F") {
+    image(fly, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+  }
+  else if (location === "P") {
+    image(player, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+  }
+  else if (location === "S") {
+    image(slime, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+  }
+  else {
+    image(floor, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+  }
+}
+
+function keyPressed() {
+  if (key === "d") {
+    movePlayer(playerX+1, playerY, playerX, playerY, "right");
+  }
+  if (key === "a") {
+    movePlayer(playerX-1, playerY, playerX, playerY, "left");
+  }
+  if (key === "s") {
+    movePlayer(playerX, playerY+1, playerX, playerY, "down");
+  }
+  if (key === "w") {
+    movePlayer(playerX, playerY-1, playerX, playerY, "up");
+  }
+}
+
+function movePlayer(x, y, oldX, oldY, direction) {
+  if (x >= 0 && x < tilesWide && y >= 0 && y < tilesHigh && tiles[y][x] !== 1) {
+    grid[y][x] = 9;
+    grid[oldY][oldX] = 0;
+
+    if (direction === "right") {
+      playerX += 1;
+    }
+    if (direction === "left") {
+      playerX -= 1;
+    }
+    if (direction === "down") {
+      playerY += 1;
+    }
+    if (direction === "up") {
+      playerY -= 1;
+    }
+  }
 }
